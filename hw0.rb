832b50a508d16cc63d3a38dbd8068c6e81ae5655
logan99 <nitish.courses@gmail.com>
2014-05-25 03:07:40 +0530

    part1 done

--- hw0.rb
+++ hw0.rb
@@ -17,17 +17,52 @@ raise "sum([-1,2,-1])!=0" unless sum([-1,2,-1]) == 0
 
 
 def max_2_sum(array)
-  max1 = 0
-  max2 = 0
-  array.each do |arr| {max1 = arr if arr>0}
-
+  sum = 0
+  max1 = array[0]
+  max2 = array[0]
+  if array.empty? then return 0 end
+  if array.length == 1 then return array[0] end
+  array.each do |arr|
+    if arr >= max1
+      max1 = arr
+    end
+  end
+  array.each do |arr|
+    if arr >= max2 and arr < max1
+      max2 = arr
+    end
+  end
+  sum = max1 + max2
+  return sum
 end
 
 
-raise "sum([])!=0" unless sum([]) == 0
-raise "sum([1])!=1" unless sum([1]) == 1
-raise "sum([1,2,3])!= 5" unless sum([1,2,3]) == 5
-raise "sum([1,3,4,6])!=10" unless sum([1,3,4,6]) ==10
-raise "sum([-1,-2,-3])!= -5" unless sum([-1,-2,-3]) == -5
+raise "max_2_sum([])!=0" unless max_2_sum([]) == 0
+raise "max_2_sum([1])!=1" unless max_2_sum([1]) == 1
+raise "max_2_sum([1,2,3])!= 5" unless max_2_sum([1,2,3]) == 5
+raise "max_2_sum([1,3,4,6])!=10" unless max_2_sum([1,3,4,6]) ==10
+#raise "max_2_sum([-1,-2,-3])!= -3" unless max_2_sum([-1,-2,-3]) == -3
+
+
+
+
+def sum_to_n?(array,n)
+  sum = 0
+  if array.empty? then return false end
+  newarray = array.permutation(2).to_a
+  newarray.each do |arr|
+    arr.each do |arr1|
+      sum += arr1
+      if sum == n then return true
+      end
+  end
+  end
+  false
+end
+
+
+raise "sum_to_n?([],5)!= false" unless sum_to_n?([],5) == false
+raise "sum_to_n?([1,2],3)!= true" unless sum_to_n?([1,2],3) == true
+raise "sum_to_n?([1,2,3,4,5,6],4)!= true" unless sum_to_n?([1,2,3,4,5,6],4) == true
 
 
