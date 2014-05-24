898d8124ca062450016df4d7a4900667378e79e8
logan99 <nitish.courses@gmail.com>
2014-05-24 23:38:13 +0530

    added class note

--- hw0.rb
+++ hw0.rb
@@ -8,6 +8,26 @@ def sum (array)
   sum
 end
 
-raise "sum[]!=0" unless sum[] == 0
-raise "sum[1,2,3]!=6" unless sum[1,2,3] == 6
-raise "sum[-1,2,-1]!=0" unless sum[-1,2,-1] == 0
+
+
+
+raise "sum([])!=0" unless sum([]) == 0
+raise "sum([1,2,3])!=6" unless sum([1,2,3]) == 6
+raise "sum([-1,2,-1])!=0" unless sum([-1,2,-1]) == 0
+
+
+def max_2_sum(array)
+  max1 = 0
+  max2 = 0
+  array.each do |arr| {max1 = arr if arr>0}
+
+end
+
+
+raise "sum([])!=0" unless sum([]) == 0
+raise "sum([1])!=1" unless sum([1]) == 1
+raise "sum([1,2,3])!= 5" unless sum([1,2,3]) == 5
+raise "sum([1,3,4,6])!=10" unless sum([1,3,4,6]) ==10
+raise "sum([-1,-2,-3])!= -5" unless sum([-1,-2,-3]) == -5
+
+
